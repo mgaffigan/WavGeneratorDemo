20b64efa9a32f258652aee0e20b1c833cea4cb9f
Mitch Gaffigan <mitch.gaffigan@comcast.net>
2019-07-26 15:38:51 -0500

    Refactoring

--- WavGeneratorDemo/Program.cs
+++ WavGeneratorDemo/Program.cs
@@ -10,61 +10,65 @@
             const int INT24_MAX = 0x7f_ffff;
 
             static void Main(string[] args)
+        {
+            const int sampleRate = 44100;
+            const int lengthInSeconds = 15 /* sec */;
+            const int channels = 2;
+            const double channelSamplesPerSecond = sampleRate * channels;
+            var samples = new double[lengthInSeconds * sampleRate * channels];
+
+            // Left is 440 Hz sine wave
+            FillWithSineWave(samples, channels, channelSamplesPerSecond, 0 /* Left */, 440 /* Hz */);
+            // Right is 1 kHz sine wave
+            FillWithSineWave(samples, channels, channelSamplesPerSecond, 1 /* Right */, 1000 /* Hz */);
+
+            WriteWavFile(samples, sampleRate, channels, "out.wav");
+        }
+
+        private static void WriteWavFile(double[] samples, uint sampleRate, ushort channels, string fileName)
+        {
+            using (var wavFile = File.OpenWrite(fileName))
             {
-                const int sampleRate = 44100;
-                const int lengthInSeconds = 15 /* sec */;
-                const int channels = 2;
-                const double channelSamplesPerSecond = sampleRate * channels;
-                var samples = new int[lengthInSeconds * sampleRate * channels];
+                const int chunkHeaderSize = 8,
+                    waveHeaderSize = 4,
+                    fmtChunkSize = 16;
+                uint samplesByteLength = (uint)samples.Length * 3u;
 
-                // Left is 440 Hz sine wave
-                FillWithSineWave(samples, channels, channelSamplesPerSecond, 0 /* Left */, 440 /* Hz */);
-                // Right is 1 kHz sine wave
-                FillWithSineWave(samples, channels, channelSamplesPerSecond, 1 /* Right */, 1000 /* Hz */);
+                // RIFF header
+                wavFile.WriteAscii("RIFF");
+                wavFile.WriteLittleEndianUInt32(
+                    waveHeaderSize
+                    + chunkHeaderSize + fmtChunkSize
+                    + chunkHeaderSize + samplesByteLength);
+                wavFile.WriteAscii("WAVE");
 
-                using (var wavFile = File.OpenWrite("out.wav"))
+                // fmt header
+                wavFile.WriteAscii("fmt ");
+                wavFile.WriteLittleEndianUInt32(fmtChunkSize);
+                wavFile.WriteLittleEndianUInt16(1);               // AudioFormat = PCM
+                wavFile.WriteLittleEndianUInt16(channels);
+                wavFile.WriteLittleEndianUInt32(sampleRate);
+                wavFile.WriteLittleEndianUInt32(sampleRate * channels);
+                wavFile.WriteLittleEndianUInt16((ushort)(3 * channels));    // Block Align (stride)
+                wavFile.WriteLittleEndianUInt16(24);              // Bits per sample
+
+                // samples data
+                wavFile.WriteAscii("data");
+                wavFile.WriteLittleEndianUInt32(samplesByteLength);
+                for (int i = 0; i < samples.Length; i++)
                 {
-                    const int chunkHeaderSize = 8,
-                        waveHeaderSize = 4,
-                        fmtChunkSize = 16;
-                    uint samplesByteLength = (uint)samples.Length * 3u;
-
-                    // RIFF header
-                    wavFile.WriteAscii("RIFF");
-                    wavFile.WriteLittleEndianUInt32(
-                        waveHeaderSize
-                        + chunkHeaderSize + fmtChunkSize
-                        + chunkHeaderSize + samplesByteLength);
-                    wavFile.WriteAscii("WAVE");
-
-                    // fmt header
-                    wavFile.WriteAscii("fmt ");
-                    wavFile.WriteLittleEndianUInt32(fmtChunkSize);
-                    wavFile.WriteLittleEndianUInt16(1);               // AudioFormat = PCM
-                    wavFile.WriteLittleEndianUInt16(channels);
-                    wavFile.WriteLittleEndianUInt32(sampleRate);
-                    wavFile.WriteLittleEndianUInt32(sampleRate * channels);
-                    wavFile.WriteLittleEndianUInt16(3 * channels);    // Block Align (stride)
-                    wavFile.WriteLittleEndianUInt16(24);              // Bits per sample
-
-                    // samples data
-                    wavFile.WriteAscii("data");
-                    wavFile.WriteLittleEndianUInt32(samplesByteLength);
-                    for (int i = 0; i < samples.Length; i++)
-                    {
-                        wavFile.WriteLittleEndianInt24(samples[i]);
-                    }
+                    var scaledValue = DoubleToInt24(samples[i]);
+                    wavFile.WriteLittleEndianInt24(scaledValue);
                 }
             }
+        }
 
-            private static void FillWithSineWave(int[] samples, int channels, double channelSamplesPerSecond, int channelNo, double freq)
+        private static void FillWithSineWave(double[] samples, int channels, double channelSamplesPerSecond, int channelNo, double freq)
             {
                 for (int i = channelNo; i < samples.Length; i += channels)
                 {
                     var t = (i - channelNo) / channelSamplesPerSecond;
-                    var value = Math.Sin(t * (freq * Math.PI * 2));
-
-                    samples[i] = DoubleToInt24(value);
+                    samples[i] = Math.Sin(t * (freq * Math.PI * 2));
                 }
             }
 
